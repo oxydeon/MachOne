8a6a872667968c734341095481e095acd4a30f08
oxydeon <oxydeon@gmail.com>
2025-01-18 22:09:12 +0100

    feat(front): refresh devices

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,8 +1,8 @@
 /* eslint-disable no-param-reassign */
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
-import { delay, filter, map, switchMap, tap } from 'rxjs';
-import { deviceValue, devicesList, waitingDelay } from './config';
+import { delay, switchMap, tap } from 'rxjs';
+import { deviceValue, devicesList, refreshDelay, waitingDelay } from './config';
 import { Api } from './core/api/services/api.service';
 import { CoreModule } from './core/core.module';
 import { DeviceApiService } from './shared/api/services/device-api.service';
@@ -31,25 +31,28 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     // retrieve credentials from query params
-    this.route.queryParams
-      .pipe(
-        // set credentials
-        map((params) => {
-          this.api.auth(params['appKey'], params['secretKey']);
-          return params;
-        }),
-        // stop if credentials are not set
-        filter((params) => !!params['appKey'] && !!params['secretKey']),
-        // retrieve devices
-        switchMap(() => this.deviceApiService.getDevices(Object.values(devicesList))),
-      )
-      .subscribe((devices) => {
-        // order devices
-        const devicesOrder = Object.keys(devicesList);
-        this.devices = devices.sort(
-          (a, b) => devicesOrder.indexOf(a.name.trim()) - devicesOrder.indexOf(b.name.trim()),
-        );
-      });
+    this.route.queryParams.subscribe((params) => {
+      if (!params['appKey'] || !params['secretKey']) return;
+
+      this.api.auth(params['appKey'], params['secretKey']);
+
+      this.retrieveDevices();
+
+      setInterval(
+        () => this.retrieveDevices(),
+        refreshDelay * 1000,
+      );
+    });
+  }
+
+  retrieveDevices(): void {
+    this.deviceApiService.getDevices(Object.values(devicesList)).subscribe((devices) => {
+      // order devices
+      const devicesOrder = Object.keys(devicesList);
+      this.devices = devices.sort(
+        (a, b) => devicesOrder.indexOf(a.name.trim()) - devicesOrder.indexOf(b.name.trim()),
+      );
+    });
   }
 
   toggleLight(device: any): void {
