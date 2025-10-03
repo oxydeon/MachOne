fbea620376eb60864208af08c3ce14ce051a0c71
oxydeon <oxydeon@gmail.com>
2025-10-03 14:41:24 +0200

    refactor(architecture): move main page to separate component

--- front/src/app/core/core.module.ts
+++ front/src/app/core/core.module.ts
@@ -1,9 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { Api } from './api/services/api.service';
 
 @NgModule({
-  exports: [CommonModule],
+  exports: [
+    CommonModule,
+    RouterModule,
+  ],
   providers: [Api],
 })
 export class CoreModule { }
