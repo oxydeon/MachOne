beb718860da3a68b52b2987f1281c02a3f01afd5
oxydeon <oxydeon@gmail.com>
2025-10-03 14:41:24 +0200

    refactor(architecture): move main page to separate component

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,131 +1,22 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Component, HostListener, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterModule } from '@angular/router';
-import { catchError } from 'rxjs';
-import { environment } from '../env';
-import { deviceTypes } from './config';
-import { Api } from './core/api/services/api.service';
+import { Component, HostListener } from '@angular/core';
 import { CoreModule } from './core/core.module';
 import { VibrationService } from './core/services/vibration.service';
-import { DeviceDimmerSwitchComponent } from './modules/devices/components/dimmer-switch/dimmer-switch.component';
-import { DeviceLightComponent } from './modules/devices/components/light/light.component';
-import { DeviceSocketComponent } from './modules/devices/components/socket/socket.component';
-import { DeviceThermometerComponent } from './modules/devices/components/thermometer/thermometer.component';
-import { DeviceUnknownComponent } from './modules/devices/components/unknown/unknown.component';
-import { DeviceValveComponent } from './modules/devices/components/valve/valve.component';
-import { Device } from './shared/api/models/device.model';
-import { DimmerSwitchDevice } from './shared/api/models/dimmer-switch.model';
-import { LightDevice } from './shared/api/models/light.model';
-import { SocketDevice } from './shared/api/models/socket.model';
-import { ThermometerDevice } from './shared/api/models/thermometer.model';
-import { ValveDevice } from './shared/api/models/valve.model';
-import { DeviceApiService } from './shared/api/services/device-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  providers: [
-    Api,
-    DeviceApiService,
-    VibrationService,
-  ],
-  imports: [
-    CoreModule,
-    RouterModule,
-    DeviceUnknownComponent,
-    DeviceSocketComponent,
-    DeviceLightComponent,
-    DeviceValveComponent,
-    DeviceThermometerComponent,
-    DeviceDimmerSwitchComponent,
-  ],
+  providers: [VibrationService],
+  imports: [CoreModule],
 })
-export class AppComponent implements OnInit {
-  baseUrl = window.location.origin;
-  deviceTypes = deviceTypes;
-
-  devicesIds: string[] = [];
-  lineBreaks: number[] = [];
-  devices?: Device[];
-  errorMessage?: string;
-  errorCount = 0;
+export class AppComponent {
 
   constructor(
-    private route: ActivatedRoute,
-    public api: Api,
-    private deviceApiService: DeviceApiService,
     private vibrationService: VibrationService,
   ) { }
 
   @HostListener('document:click', ['$event'])
   onDocumentClick(): void {
     this.vibrationService.vibrate(50);
-  }
-
-  ngOnInit(): void {
-    // retrieve credentials from query params
-    this.route.queryParams.subscribe(({ appKey, secretKey, devices, lineBreaks }) => {
-      if (!appKey || !secretKey || !devices) {
-        this.api.clearAuth();
-        return;
-      }
-
-      this.api.auth(appKey, secretKey);
-      this.devicesIds = devices.split(',');
-      this.lineBreaks = lineBreaks ? lineBreaks.split(',').map(Number) : [];
-
-      this.retrieveDevices();
-
-      setInterval(
-        () => this.retrieveDevices(),
-        environment.refreshDelay * 1000,
-      );
-    });
-  }
-
-  retrieveDevices(): void {
-    if (this.errorCount >= environment.retryCount) return;
-
-    this.deviceApiService.getDevices(this.devicesIds)
-      .pipe(
-        catchError((e: HttpErrorResponse) => {
-          const { error, message } = e.error;
-          this.errorMessage = error && message ? `${error} - ${message}` : 'An error occurred';
-          this.errorCount += 1;
-
-          return [];
-        }),
-      )
-      .subscribe((devices) => {
-        // clear error
-        this.errorMessage = undefined;
-        this.errorCount = 0;
-
-        // order devices
-        this.devices = devices.sort(
-          (a, b) => this.devicesIds.indexOf(a.id) - this.devicesIds.indexOf(b.id),
-        );
-      });
-  }
-
-  isSocketDevice(device: Device): device is SocketDevice {
-    return this.deviceTypes.socket.includes(device.category);
-  }
-
-  isLightDevice(device: Device): device is LightDevice {
-    return this.deviceTypes.light.includes(device.category);
-  }
-
-  isValveDevice(device: Device): device is ValveDevice {
-    return this.deviceTypes.valve.includes(device.category);
-  }
-
-  isThermometerDevice(device: Device): device is ThermometerDevice {
-    return this.deviceTypes.thermometer.includes(device.category);
-  }
-
-  isDimmerSwitchDevice(device: Device): device is DimmerSwitchDevice {
-    return this.deviceTypes.dimmerSwitch.includes(device.category);
   }
 }
---
+++ front/src/app/modules/devices/pages/devices/devices.component.ts
@@ -0,0 +1,122 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { catchError } from 'rxjs';
+import { environment } from '../../../../../env';
+import { deviceTypes } from '../../../../config';
+import { Api } from '../../../../core/api/services/api.service';
+import { CoreModule } from '../../../../core/core.module';
+import { Device } from '../../../../shared/api/models/device.model';
+import { DimmerSwitchDevice } from '../../../../shared/api/models/dimmer-switch.model';
+import { LightDevice } from '../../../../shared/api/models/light.model';
+import { SocketDevice } from '../../../../shared/api/models/socket.model';
+import { ThermometerDevice } from '../../../../shared/api/models/thermometer.model';
+import { ValveDevice } from '../../../../shared/api/models/valve.model';
+import { DeviceApiService } from '../../../../shared/api/services/device-api.service';
+import { DeviceDimmerSwitchComponent } from '../../components/dimmer-switch/dimmer-switch.component';
+import { DeviceLightComponent } from '../../components/light/light.component';
+import { DeviceSocketComponent } from '../../components/socket/socket.component';
+import { DeviceThermometerComponent } from '../../components/thermometer/thermometer.component';
+import { DeviceUnknownComponent } from '../../components/unknown/unknown.component';
+import { DeviceValveComponent } from '../../components/valve/valve.component';
+
+@Component({
+  selector: 'app-devices',
+  templateUrl: './devices.component.html',
+  standalone: true,
+  providers: [
+    Api,
+    DeviceApiService,
+  ],
+  imports: [
+    CoreModule,
+    DeviceUnknownComponent,
+    DeviceSocketComponent,
+    DeviceLightComponent,
+    DeviceValveComponent,
+    DeviceThermometerComponent,
+    DeviceDimmerSwitchComponent,
+  ],
+})
+export class DevicesComponent implements OnInit {
+  baseUrl = window.location.origin;
+  deviceTypes = deviceTypes;
+
+  devicesIds: string[] = [];
+  lineBreaks: number[] = [];
+  devices?: Device[];
+  errorMessage?: string;
+  errorCount = 0;
+
+  constructor(
+    private route: ActivatedRoute,
+    public api: Api,
+    private deviceApiService: DeviceApiService,
+  ) { }
+
+  ngOnInit(): void {
+    // retrieve credentials from query params
+    this.route.queryParams.subscribe(({ appKey, secretKey, devices, lineBreaks }) => {
+      if (!appKey || !secretKey || !devices) {
+        this.api.clearAuth();
+        return;
+      }
+
+      this.api.auth(appKey, secretKey);
+      this.devicesIds = devices.split(',');
+      this.lineBreaks = lineBreaks ? lineBreaks.split(',').map(Number) : [];
+
+      this.retrieveDevices();
+
+      setInterval(
+        () => this.retrieveDevices(),
+        environment.refreshDelay * 1000,
+      );
+    });
+  }
+
+  retrieveDevices(): void {
+    if (this.errorCount >= environment.retryCount) return;
+
+    this.deviceApiService.getDevices(this.devicesIds)
+      .pipe(
+        catchError((e: HttpErrorResponse) => {
+          const { error, message } = e.error;
+          this.errorMessage = error && message ? `${error} - ${message}` : 'An error occurred';
+          this.errorCount += 1;
+
+          return [];
+        }),
+      )
+      .subscribe((devices) => {
+        // clear error
+        this.errorMessage = undefined;
+        this.errorCount = 0;
+
+        // order devices
+        this.devices = devices.sort(
+          (a, b) => this.devicesIds.indexOf(a.id) - this.devicesIds.indexOf(b.id),
+        );
+      });
+  }
+
+  isSocketDevice(device: Device): device is SocketDevice {
+    return this.deviceTypes.socket.includes(device.category);
+  }
+
+  isLightDevice(device: Device): device is LightDevice {
+    return this.deviceTypes.light.includes(device.category);
+  }
+
+  isValveDevice(device: Device): device is ValveDevice {
+    return this.deviceTypes.valve.includes(device.category);
+  }
+
+  isThermometerDevice(device: Device): device is ThermometerDevice {
+    return this.deviceTypes.thermometer.includes(device.category);
+  }
+
+  isDimmerSwitchDevice(device: Device): device is DimmerSwitchDevice {
+    return this.deviceTypes.dimmerSwitch.includes(device.category);
+  }
+}
